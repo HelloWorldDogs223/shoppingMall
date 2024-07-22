105929b6296bc36284329de6bd58c9451bfbfacb
yong hyeok <yonghyeogi377@gmail.com>
2024-07-22 14:36:15 +0900

    Init project

---
+++ app/cart/page.tsx
@@ -0,0 +1,148 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col justify-center px-[196px]">
+      <div className="flex justify-center mb-[24px] mt-[40px]">
+        <div className=" text-neutral-900 text-[22px] font-bold font-['Work Sans'] leading-7">
+          Shopping Cart
+        </div>
+      </div>
+      <div className="flex justify-between items-center mb-[32px]">
+        <div className="flex justify-center">
+          <img
+            src="/example4.png"
+            className="w-[70px] h-[70px]"
+            alt="product"
+          />
+          <div className="flex flex-col ml-[16px]">
+            <div className="w-[142px] text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              Nike Air Max 1
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              $180.00
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              Size 7.5, Color: Black
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex mr-[32px] cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              -
+            </div>
+          </div>
+          <span className="mr-[32px]">1</span>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              +
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mb-[32px]">
+        <div className="flex justify-center">
+          <img
+            src="/example4.png"
+            className="w-[70px] h-[70px]"
+            alt="product"
+          />
+          <div className="flex flex-col ml-[16px]">
+            <div className="w-[142px] text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              Nike Air Max 1
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              $180.00
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              Size 7.5, Color: Black
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex mr-[32px] cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              -
+            </div>
+          </div>
+          <span className="mr-[32px]">1</span>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              +
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="flex justify-center">
+          <img
+            src="/example4.png"
+            className="w-[70px] h-[70px]"
+            alt="product"
+          />
+          <div className="flex flex-col ml-[16px]">
+            <div className="w-[142px] text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              Nike Air Max 1
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              $180.00
+            </div>
+            <div className="w-[142px] text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              Size 7.5, Color: Black
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex mr-[32px] cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              -
+            </div>
+          </div>
+          <span className="mr-[32px]">1</span>
+          <div className="w-7 h-7 bg-gray-100 rounded-[14px] justify-center items-center inline-flex cursor-pointer">
+            <div className="text-center text-neutral-900 text-base font-medium font-['Work Sans'] leading-normal">
+              +
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[150px] w-full">
+        <div className="w-full py-2 justify-between items-start inline-flex">
+          <div className="flex-col justify-start items-start inline-flex">
+            <div className="text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              Subtotal (3 items)
+            </div>
+          </div>
+          <div className="flex-col justify-start  items-start inline-flex">
+            <div className="text-right text-neutral-900 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              $540.00
+            </div>
+          </div>
+        </div>
+        <div className="w-full py-2 justify-between items-start inline-flex">
+          <div className="flex-col justify-start items-start inline-flex">
+            <div className="text-slate-500 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              Estimated Tax
+            </div>
+          </div>
+          <div className="flex-col justify-start items-start inline-flex">
+            <div className="text-right text-neutral-900 text-sm font-normal font-['Work Sans'] leading-[21px]">
+              To be determined
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-end mt-[36px]">
+        <div className="text-center text-neutral-900 text-base font-bold font-['Work Sans'] leading-normal mr-[32px] cursor-pointer">
+          Continue Shopping
+        </div>
+        <div className="w-[206px] h-12 px-5 bg-blue-500 rounded-xl justify-center items-center inline-flex cursor-pointer">
+          <div className="flex-col justify-start items-center inline-flex">
+            <div className="text-center text-white text-base font-bold font-['Work Sans'] leading-normal">
+              Proceed to Checkout
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,32 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full pb-[100px]">
+      <div className="flex mt-[36px] px-[176px] mb-[32px]">
+        <div className="relative flex-1 flex justify-center">
+          <img
+            src="/example.jpg"
+            className="w-[928px] h-[400px] object-fill  rounded-2xl"
+            alt="main-image"
+          />
+          <div className="absolute top-[320px] z-[2] left-[50%] translate-x-[255%] w-[119px] h-12 px-5 bg-black rounded-xl justify-center items-center inline-flex cursor-pointer">
+            <div className="flex-col justify-start items-center inline-flex">
+              <div className="text-center text-white text-base font-bold font-['Work Sans'] leading-normal">
+                Shop now
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center mt-[32px] mb-[32px]">
+        <img src="/example2.png" alt="main-image2" className="mr-[12px]" />
+        <img src="/example3.png" alt="main-image3" />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-center">
+        <img src="/example4.png" className="mr-[12px]" alt="main-image3" />
+        <img src="/example4.png" className="mr-[12px]" alt="main-image3" />
+        <img src="/example4.png" className="mr-[12px]" alt="main-image3" />
+        <img src="/example4.png" className="mr-[12px]" alt="main-image3" />
       </div>
-    </main>
+    </div>
   );
 }
