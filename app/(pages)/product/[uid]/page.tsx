1b6e0261c9a7ecc976b7a25ae647447d00dd9a80
yong hyeok <yonghyeogi377@gmail.com>
2024-07-22 15:58:55 +0900

    product published

--- app/(pages)/product/[uid]/page.tsx
+++ app/(pages)/product/[uid]/page.tsx
@@ -1,29 +1,106 @@
-import { Button } from '@mui/material';
+'use client';
+
+import {
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Rating,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
+import { useState } from 'react';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = ['tv', '3구 콘센트', '케이블', '스위치 케이스', '플러그'];
 
 export default function Page() {
+  const [age, setAge] = useState('선택');
+
+  const [personName, setPersonName] = useState<string[]>([]);
+
+  const handleChangeMultiple = (
+    event: SelectChangeEvent<typeof personName>,
+  ) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
-    <div className="flex">
+    <div className="flex pt-[32px] h-screen">
       <img src="/example5.png" alt="product-image" />
       <div>
-        <div>닌텐도</div>
-        <div>[닌텐도 스위치] 게임 타이틀</div>
-        <div>별점</div>
-        <div>가격</div>
-        <div>타이틀 선택</div>
-        <select name="languages" id="lang">
-          <option value="javascript">JavaScript</option>
-          <option value="php">PHP</option>
-          <option value="java">Java</option>
-          <option value="golang">Golang</option>
-          <option value="python">Python</option>
-          <option value="c#">C#</option>
-          <option value="C++">C++</option>
-          <option value="erlang">Erlang</option>
-        </select>
+        <h1 className="text-[50px] font-bold mb-[10px]">닌텐도</h1>
+        <h2 className="text-[30px] font-semibold mb-[16px]">
+          [닌텐도 스위치] 게임 타이틀
+        </h2>
+        <Rating name="read-only" value={2} readOnly className="mb-[32px]" />
+        <div className="flex justify-between">
+          <div className="text-[30px] font-semibold mb-[100px]">가격</div>
+          <div className="font-semibold text-[30px]">5000000000원</div>
+        </div>
+
+        <div className="w-[500px]">
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">색상</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>하얀색 (+5000)</MenuItem>
+              <MenuItem value={20}>빨간색 (+3000)</MenuItem>
+              <MenuItem value={30}>검은색 (+4000)</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="w-full mt-[120px]">
+          <FormControl sx={{ m: 1, width: 500 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+            <Select
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              multiple
+              value={personName}
+              onChange={handleChangeMultiple}
+              input={<OutlinedInput label="Tag" />}
+              renderValue={(selected) => selected.join(', ')}
+              MenuProps={MenuProps}
+            >
+              {names.map((name) => (
+                <MenuItem key={name} value={name}>
+                  <Checkbox checked={personName.indexOf(name) > -1} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
       </div>
-      <Button className="w-[100px] h-[100px]" variant="contained">
-        하이
-      </Button>
     </div>
   );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,71 +11,73 @@ export default function Home() {
   };
 
   return (
-    <div className="flex justify-between px-10 py-3 border-b border-gray-200 border-solid items-center bg-gray-100">
-      <div className="text-neutral-900 text-lg font-bold ">
-        <h1 className="cursor-pointer">
-          <Link href="/">JY Shopping Mall</Link>
-        </h1>
-      </div>
-      <div className="flex items-center">
-        <div className="flex relative mr-[32px]">
-          <input
-            className="w-[300px] h-[40px] border-solid border-gray-600 border rounded-md px-[50px]"
-            placeholder="검색어를 입력하세요."
-          />
-          <svg
-            className="absolute left-[10px] top-[10px] cursor-pointer"
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-          >
-            <path
-              id="Vector - 0"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M19.5306 18.4694L14.8366 13.7762C17.6629 10.383 17.3204 5.36693 14.0591 2.38935C10.7978 -0.588237 5.77134 -0.474001 2.64867 2.64867C-0.474001 5.77134 -0.588237 10.7978 2.38935 14.0591C5.36693 17.3204 10.383 17.6629 13.7762 14.8366L18.4694 19.5306C18.7624 19.8237 19.2376 19.8237 19.5306 19.5306C19.8237 19.2376 19.8237 18.7624 19.5306 18.4694ZM1.75 8.5C1.75 4.77208 4.77208 1.75 8.5 1.75C12.2279 1.75 15.25 4.77208 15.25 8.5C15.25 12.2279 12.2279 15.25 8.5 15.25C4.77379 15.2459 1.75413 12.2262 1.75 8.5Z"
-              fill="#637587"
-            />
-          </svg>
+    <div className="relative mb-[100px] ">
+      <div className="fixed top-0 left-0 z-[101] w-full flex justify-between px-10 py-3 border-b border-gray-200 border-solid items-center bg-gray-100">
+        <div className="text-neutral-900 text-lg font-bold ">
+          <h1 className="cursor-pointer">
+            <Link href="/">JY Shopping Mall</Link>
+          </h1>
         </div>
-        <Button
-          onClick={onClickHandler}
-          variant="contained"
-          className="w-[94px] h-10 px-4 bg-blue-500 rounded-xl justify-center items-center inline-flex cursor-pointer mr-[32px]"
-        >
-          <div className="flex-col justify-start items-center inline-flex">
-            <div className="text-center text-white text-sm font-bold font-['Work Sans'] leading-[21px]">
-              Sign in
+        <div className="flex items-center">
+          <div className="flex relative mr-[32px]">
+            <input
+              className="w-[300px] h-[40px] border-solid border-gray-600 border rounded-md px-[50px]"
+              placeholder="검색어를 입력하세요."
+            />
+            <svg
+              className="absolute left-[10px] top-[10px] cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                id="Vector - 0"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M19.5306 18.4694L14.8366 13.7762C17.6629 10.383 17.3204 5.36693 14.0591 2.38935C10.7978 -0.588237 5.77134 -0.474001 2.64867 2.64867C-0.474001 5.77134 -0.588237 10.7978 2.38935 14.0591C5.36693 17.3204 10.383 17.6629 13.7762 14.8366L18.4694 19.5306C18.7624 19.8237 19.2376 19.8237 19.5306 19.5306C19.8237 19.2376 19.8237 18.7624 19.5306 18.4694ZM1.75 8.5C1.75 4.77208 4.77208 1.75 8.5 1.75C12.2279 1.75 15.25 4.77208 15.25 8.5C15.25 12.2279 12.2279 15.25 8.5 15.25C4.77379 15.2459 1.75413 12.2262 1.75 8.5Z"
+                fill="#637587"
+              />
+            </svg>
+          </div>
+          <Button
+            onClick={onClickHandler}
+            variant="contained"
+            className="w-[94px] h-10 px-4 bg-blue-500 rounded-xl justify-center items-center inline-flex cursor-pointer mr-[32px]"
+          >
+            <div className="flex-col justify-start items-center inline-flex">
+              <div className="text-center text-white text-sm font-bold font-['Work Sans'] leading-[21px]">
+                Sign in
+              </div>
+            </div>
+          </Button>
+          <div className="w-[84px] h-10 px-4 bg-gray-200 rounded-xl justify-center items-center inline-flex cursor-pointer mr-[32px]">
+            <div className="flex-col justify-start items-center inline-flex">
+              <div className="text-center text-neutral-900 text-sm font-bold font-['Work Sans'] leading-[21px]">
+                Help
+              </div>
             </div>
           </div>
-        </Button>
-        <div className="w-[84px] h-10 px-4 bg-gray-200 rounded-xl justify-center items-center inline-flex cursor-pointer mr-[32px]">
-          <div className="flex-col justify-start items-center inline-flex">
-            <div className="text-center text-neutral-900 text-sm font-bold font-['Work Sans'] leading-[21px]">
-              Help
-            </div>
-          </div>
+          <Link href="/cart">
+            <svg
+              className="cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="14"
+              viewBox="0 0 18 14"
+              fill="none"
+            >
+              <path
+                id="Vector - 0"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M15.875 0.125H2.125C1.43464 0.125 0.875 0.684644 0.875 1.375V12.625C0.875 13.3154 1.43464 13.875 2.125 13.875H15.875C16.5654 13.875 17.125 13.3154 17.125 12.625V1.375C17.125 0.684644 16.5654 0.125 15.875 0.125ZM15.875 12.625H2.125V1.375H15.875V12.625ZM12.75 3.875C12.75 5.94607 11.0711 7.625 9 7.625C6.92893 7.625 5.25 5.94607 5.25 3.875C5.25 3.52982 5.52982 3.25 5.875 3.25C6.22018 3.25 6.5 3.52982 6.5 3.875C6.5 5.25571 7.61929 6.375 9 6.375C10.3807 6.375 11.5 5.25571 11.5 3.875C11.5 3.52982 11.7798 3.25 12.125 3.25C12.4702 3.25 12.75 3.52982 12.75 3.875Z"
+                fill="#121417"
+              />
+            </svg>
+          </Link>
         </div>
-        <Link href="/cart">
-          <svg
-            className="cursor-pointer"
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="14"
-            viewBox="0 0 18 14"
-            fill="none"
-          >
-            <path
-              id="Vector - 0"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M15.875 0.125H2.125C1.43464 0.125 0.875 0.684644 0.875 1.375V12.625C0.875 13.3154 1.43464 13.875 2.125 13.875H15.875C16.5654 13.875 17.125 13.3154 17.125 12.625V1.375C17.125 0.684644 16.5654 0.125 15.875 0.125ZM15.875 12.625H2.125V1.375H15.875V12.625ZM12.75 3.875C12.75 5.94607 11.0711 7.625 9 7.625C6.92893 7.625 5.25 5.94607 5.25 3.875C5.25 3.52982 5.52982 3.25 5.875 3.25C6.22018 3.25 6.5 3.52982 6.5 3.875C6.5 5.25571 7.61929 6.375 9 6.375C10.3807 6.375 11.5 5.25571 11.5 3.875C11.5 3.52982 11.7798 3.25 12.125 3.25C12.4702 3.25 12.75 3.52982 12.75 3.875Z"
-              fill="#121417"
-            />
-          </svg>
-        </Link>
       </div>
     </div>
   );
