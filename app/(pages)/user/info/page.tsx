292746dd221e18f2c5242efbef6560c08f5bf05b
yong hyeok <yonghyeogi377@gmail.com>
2024-07-28 13:53:18 +0900

    문제해결

--- app/(pages)/user/info/page.tsx
+++ app/(pages)/user/info/page.tsx
@@ -35,6 +35,9 @@ export default function Page() {
       alert('시간이 아직 남아있습니다.');
       return;
     }
+
+    console.log('asfssf' + process.env.NEXT_PUBLIC_SERVER_DOMAIN);
+
     const res: any = await axios.post(
       `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/email/registration/request`,
       { email },
