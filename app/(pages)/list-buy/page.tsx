30b284d108b0c6b26c6f99686396ecebf4920902
yong hyeok <yonghyeogi377@gmail.com>
2024-08-12 10:56:08 +0900

    asdasd

--- app/(pages)/list-buy/page.tsx
+++ app/(pages)/list-buy/page.tsx
@@ -34,6 +34,9 @@ export default function Page() {
       },
       { headers: { Authorization: `Bearer ${accessToken}` } },
     );
+
+    console.log(refundRes);
+
     if (refundRes.data.refundId) {
       alert('환불 요청이 완료되었습니다.');
     }
@@ -41,49 +44,56 @@ export default function Page() {
 
   useEffect(() => {
     getList();
-  }, []);
+  }, [accessToken]);
 
   return (
     <div>
       {list.map((el: any) => {
         return (
           <div className="pl-[100px] flex flex-col gap-4">
-            <div>{el.purchaseId}</div>
-            <div>{el.state}</div>
-            <div>{el.purchaseTitle}</div>
-            <div>{el.totalPrice}</div>
-            <div>{el.dateTime}</div>
+            <div>구매 ID : {el.purchaseId}</div>
+            <div>구매 상태 : {el.state}</div>
+            <div>구매 제목 : {el.purchaseTitle}</div>
+            <div>최종 가격 : {el.totalPrice}</div>
+            <div>시간 : {el.dateTime}</div>
             <div>
               {el.purchaseItems.map((item: any) => {
                 return (
                   <>
-                    <div>{item.productName}</div>
-                    <div>{item.productTypeName}</div>
-                    <div>{item.selectedSingleOption.optionName}</div>
-                    <div>{item.selectedSingleOption.priceChangeAmount}</div>
-                    <div>{item.price}</div>
-                    <div>{item.discountAmount}</div>
-                    <div>{item.discountRate}</div>
-                    <div>{item.finalPrice}</div>
-                    <div>{item.isRefund}</div>
-                    <input
-                      className="w-[480px] border-solid border border-red-500"
-                      placeholder="환불 요청 제목"
-                      value={requestTitle}
-                      onChange={(e) => setRequestTitle(e.target.value)}
-                    />
-                    <textarea
-                      className="w-[480px] border-solid border border-red-500 resize-none"
-                      value={requestContent}
-                      placeholder="환불 요청 내용"
-                      onChange={(e) => setRequestContent(e.target.value)}
-                    />
-                    <Button
-                      variant="contained"
-                      onClick={() => getRefund(el.purchaseId)}
-                    >
-                      환불 요청하기
-                    </Button>
+                    <div>제품 이름 : {item.productName}</div>
+                    <div>제품 타입 : {item.productTypeName}</div>
+                    <div>
+                      단일 옵션 : {item.selectedSingleOption.optionName}
+                    </div>
+                    <div>
+                      단일 옵션 가격 :{' '}
+                      {item.selectedSingleOption.priceChangeAmount}
+                    </div>
+                    <div>가격 : {item.price}</div>
+                    <div>할인 : {item.discountAmount}</div>
+                    <div>할인율 : {item.discountRate}</div>
+                    <div>최종 가격 : {item.finalPrice}</div>
+                    <div>환불여부 : {item.isRefund}</div>
+                    <div className="flex flex-col gap-4">
+                      <input
+                        className="w-[480px] border-solid border border-red-500"
+                        placeholder="환불 요청 제목"
+                        value={requestTitle}
+                        onChange={(e) => setRequestTitle(e.target.value)}
+                      />
+                      <textarea
+                        className="w-[480px] border-solid border border-red-500 resize-none"
+                        value={requestContent}
+                        placeholder="환불 요청 내용"
+                        onChange={(e) => setRequestContent(e.target.value)}
+                      />
+                      <Button
+                        variant="contained"
+                        onClick={() => getRefund(el.purchaseId)}
+                      >
+                        환불 요청하기
+                      </Button>
+                    </div>
                   </>
                 );
               })}
