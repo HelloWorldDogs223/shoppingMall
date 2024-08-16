02c0e244b45ff0d3b2996af739ad5cab17947274
yong hyeok <yonghyeogi377@gmail.com>
2024-08-16 11:15:50 +0900

    sadasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          type="text/javascript"
-          src="https://code.jquery.com/jquery-1.12.4.min.js"
-        ></script>
-        <script
-          type="text/javascript"
-          src="https://cdn.iamport.kr/js/iamport.payment-1.1.8.js"
-        ></script>
-      </head>
+      <head></head>
       <body className={inter.className}>
         <Header />
         {children}
