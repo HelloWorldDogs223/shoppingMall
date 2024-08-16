82638fcce93fa8bc923da728234335cf7c3c95d5
yong hyeok <yonghyeogi377@gmail.com>
2024-08-16 12:27:03 +0900

    댓글

--- app/(pages)/product/[uid]/page.tsx
+++ app/(pages)/product/[uid]/page.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+interface Block {
+  index: number; // 블록 순서
+  blockType: string;
+  contentInTextBlock: string; // 텍스트 블록을 위한
+  imgDownloadUrlInImageBlock: string;
+}
+
 interface ProductType {
   productId: number; // 제품 Id
   sellerId: number; // 판매자 Id
   productTypeId: number; // 제품타입 Id
   productImageDownloadUrlList: [];
-  blockDataList: {
-    // 블록 데이터 리스트
-    index: number; // 블록 순서
-    blockType: string;
-    contentInTextBlock: string; // 텍스트 블록을 위한
-    imgDownloadUrlInImageBlock: String;
-  };
+  blockDataList: Block[];
   singleOptions: // 단일옵션 리스트
   {
     optionName: string; // 옵션명
@@ -43,6 +44,7 @@ import {
   Rating,
   Select,
   SelectChangeEvent,
+  Typography,
 } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -75,6 +77,12 @@ export default function Page() {
   const [multi, setMulti] = useState([]);
   const [modal, setModal] = useState(false);
   const [comments, setComments] = useState([]);
+
+  const [commentTitle, setCommentTitle] = useState('');
+  const [commentContent, setCommentContent] = useState('');
+  const [commentImg, setCommentImg] = useState<string>('');
+
+  const [score, setScore] = useState<number | null>(0);
 
   const params = useParams();
 
@@ -174,6 +182,26 @@ export default function Page() {
     );
   };
 
+  const commentOnSubmitHandler = async () => {
+    if (commentImg !== '' && commentTitle !== '') {
+      axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/review`,
+        {
+          reviewData: {
+            purchaseItemId: productInfo?.productId,
+            score,
+            title: commentTitle,
+            description: commentContent,
+          },
+          reviewImage: commentImg,
+        },
+        { headers: { Authorization: `Bearer ${accessToken}` } },
+      );
+    } else {
+      alert('모두 입력해주세요!');
+    }
+  };
+
   return (
     <div
       className="relative flex size-full min-h-screen flex-col bg-[#f8fafb] group/design-root overflow-x-hidden"
@@ -208,7 +236,7 @@ export default function Page() {
                 </div>
               </div>
             </div>
-            <div className="flex gap-4 mt-4 justify-between">
+            <div className="flex gap-4 mt-4 justify-between w-[100px]">
               <div>
                 <h2 className="text-2xl">가격</h2>
                 <h3 className="text-2xl">{productInfo?.price}원</h3>
@@ -303,7 +331,21 @@ export default function Page() {
                     </svg>
                   </div>
                 </summary>
-                <p className="text-[#4f7396] text-sm font-normal leading-normal pb-2"></p>
+                <p className="text-[#4f7396] text-sm font-normal leading-normal pb-2">
+                  {productInfo?.blockDataList?.map((item: Block) => {
+                    return (
+                      <div>
+                        {item.blockType === 'TEXT_TYPE' ? (
+                          <div>{item.contentInTextBlock}</div>
+                        ) : (
+                          <div>
+                            <img src={item.imgDownloadUrlInImageBlock} />
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </p>
               </details>
               <details className="flex flex-col rounded-xl border border-[#d0dbe6] bg-[#f8fafb] px-[15px] py-[7px] group">
                 <summary className="flex cursor-pointer items-center justify-between gap-6 py-2">
@@ -357,171 +399,50 @@ export default function Page() {
             <div className="flex flex-wrap gap-x-8 gap-y-6 p-4 mt-[100px]">
               <div className="flex flex-col gap-2">
                 <p className="text-[#0e141b] text-4xl font-black leading-tight tracking-[-0.033em]">
-                  4.5
+                  {productInfo?.scoreAvg}점
                 </p>
-                <div className="flex gap-0.5">
-                  <div
-                    className="text-[#348de5]"
-                    data-icon="Star"
-                    data-size="18px"
-                    data-weight="fill"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path d="M234.5,114.38l-45.1,39.36,13.51,58.6a16,16,0,0,1-23.84,17.34l-51.11-31-51,31a16,16,0,0,1-23.84-17.34L66.61,153.8,21.5,114.38a16,16,0,0,1,9.11-28.06l59.46-5.15,23.21-55.36a15.95,15.95,0,0,1,29.44,0h0L166,81.17l59.44,5.15a16,16,0,0,1,9.11,28.06Z"></path>
-                    </svg>
-                  </div>
-                  <div
-                    className="text-[#348de5]"
-                    data-icon="Star"
-                    data-size="18px"
-                    data-weight="fill"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path d="M234.5,114.38l-45.1,39.36,13.51,58.6a16,16,0,0,1-23.84,17.34l-51.11-31-51,31a16,16,0,0,1-23.84-17.34L66.61,153.8,21.5,114.38a16,16,0,0,1,9.11-28.06l59.46-5.15,23.21-55.36a15.95,15.95,0,0,1,29.44,0h0L166,81.17l59.44,5.15a16,16,0,0,1,9.11,28.06Z"></path>
-                    </svg>
-                  </div>
-                  <div
-                    className="text-[#348de5]"
-                    data-icon="Star"
-                    data-size="18px"
-                    data-weight="fill"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path d="M234.5,114.38l-45.1,39.36,13.51,58.6a16,16,0,0,1-23.84,17.34l-51.11-31-51,31a16,16,0,0,1-23.84-17.34L66.61,153.8,21.5,114.38a16,16,0,0,1,9.11-28.06l59.46-5.15,23.21-55.36a15.95,15.95,0,0,1,29.44,0h0L166,81.17l59.44,5.15a16,16,0,0,1,9.11,28.06Z"></path>
-                    </svg>
-                  </div>
-                  <div
-                    className="text-[#348de5]"
-                    data-icon="Star"
-                    data-size="18px"
-                    data-weight="fill"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path d="M234.5,114.38l-45.1,39.36,13.51,58.6a16,16,0,0,1-23.84,17.34l-51.11-31-51,31a16,16,0,0,1-23.84-17.34L66.61,153.8,21.5,114.38a16,16,0,0,1,9.11-28.06l59.46-5.15,23.21-55.36a15.95,15.95,0,0,1,29.44,0h0L166,81.17l59.44,5.15a16,16,0,0,1,9.11,28.06Z"></path>
-                    </svg>
-                  </div>
-                  <div
-                    className="text-[#348de5]"
-                    data-icon="Star"
-                    data-size="18px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path d="M239.2,97.29a16,16,0,0,0-13.81-11L166,81.17,142.72,25.81h0a15.95,15.95,0,0,0-29.44,0L90.07,81.17,30.61,86.32a16,16,0,0,0-9.11,28.06L66.61,153.8,53.09,212.34a16,16,0,0,0,23.84,17.34l51-31,51.11,31a16,16,0,0,0,23.84-17.34l-13.51-58.6,45.1-39.36A16,16,0,0,0,239.2,97.29Zm-15.22,5-45.1,39.36a16,16,0,0,0-5.08,15.71L187.35,216v0l-51.07-31a15.9,15.9,0,0,0-16.54,0l-51,31h0L82.2,157.4a16,16,0,0,0-5.08-15.71L32,102.35a.37.37,0,0,1,0-.09l59.44-5.14a16,16,0,0,0,13.35-9.75L128,32.08l23.2,55.29a16,16,0,0,0,13.35,9.75L224,102.26S224,102.32,224,102.33Z"></path>
-                    </svg>
-                  </div>
-                </div>
+                <Rating
+                  name="half-rating-read"
+                  defaultValue={productInfo?.scoreAvg}
+                  precision={0.1}
+                  readOnly
+                />
                 <p className="text-[#0e141b] text-base font-normal leading-normal">
-                  2,000 reviews
+                  {comments.length} reviews
                 </p>
               </div>
             </div>
 
-            {/* textBlock과 imgBlock 필수 */}
-
-            {/* 여기에 댓글 목록 */}
-
+            <div>
+              <form onSubmit={commentOnSubmitHandler}>
+                <Typography component="legend">Controlled</Typography>
+                <Rating
+                  name="simple-controlled"
+                  value={score}
+                  onChange={(event: any, newValue: number | null) => {
+                    setScore(newValue);
+                  }}
+                />
+                <input
+                  value={commentTitle}
+                  onChange={(e: any) => setCommentTitle(e.target.value)}
+                />
+                <textarea
+                  value={commentContent}
+                  onChange={(e: any) => setCommentContent(e.target.value)}
+                />
+                <input
+                  type="file"
+                  value={commentImg}
+                  onChange={(e: any) => setCommentImg(e.target.value)}
+                />
+                <Button onClick={commentOnSubmitHandler}>제출하기</Button>
+              </form>
+            </div>
             <div className="relative flex size-full min-h-screen flex-col bg-[#fcfbf8] group/design-root overflow-x-hidden">
               <div className="layout-container flex h-full grow flex-col">
                 <div className="px-40 flex flex-1 justify-center py-5">
                   <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
-                    <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
-                      <div className="flex flex-col gap-3 pb-3">
-                        <div>
-                          <p className="text-[#1c190d] text-base font-medium leading-normal">
-                            Nike Air Max 97
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            $150
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            4.5 stars (2,000 reviews)
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex flex-col gap-3 pb-3">
-                        <div>
-                          <p className="text-[#1c190d] text-base font-medium leading-normal">
-                            Nike Air Max 90
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            $120
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            4.3 stars (1,500 reviews)
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex flex-col gap-3 pb-3">
-                        <div>
-                          <p className="text-[#1c190d] text-base font-medium leading-normal">
-                            Nike Air Max 95
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            $170
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            4.7 stars (3,000 reviews)
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex flex-col gap-3 pb-3">
-                        <div>
-                          <p className="text-[#1c190d] text-base font-medium leading-normal">
-                            Nike Air Max 270
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            $140
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            4.2 stars (1,800 reviews)
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex flex-col gap-3 pb-3">
-                        <div>
-                          <p className="text-[#1c190d] text-base font-medium leading-normal">
-                            Nike Air Max 720
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            $160
-                          </p>
-                          <p className="text-[#9c8d49] text-sm font-normal leading-normal">
-                            4.6 stars (2,500 reviews)
-                          </p>
-                        </div>
-                      </div>
-                    </div>
                     <h2 className="text-[#1c190d] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">
                       Rating &amp; Reviews
                     </h2>
