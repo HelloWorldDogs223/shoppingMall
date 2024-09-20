48c5c8b384154a4f2d053256634300977e44b5d8
Yong Hyeok Lee <129850728+HelloWorldDogs223@users.noreply.github.com>
2024-09-20 11:24:53 +0900

    dasdasd

--- app/(pages)/user/info/page.tsx
+++ app/(pages)/user/info/page.tsx
@@ -1,22 +1,10 @@
 'use client';
 
 import { useFetch } from '@/app/hooks/useFetch';
-import {
-  Button,
-  TextField,
-  Typography,
-  Avatar,
-  Grid,
-  Card,
-  CardContent,
-  CardActions,
-  IconButton,
-  Paper,
-  Badge,
-} from '@mui/material';
-import { Edit, Email, Save, PhotoCamera } from '@mui/icons-material';
+import { Button } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
 import apiClient from '@/app/utils/axiosSetting';
 
 export default function Page() {
@@ -34,10 +22,10 @@ export default function Page() {
   const [imgFile, setImgFile] = useState<File | null>(null);
   const [emailError, setEmailError] = useState(false);
 
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null); // HTMLInputElement로 타입 지정
 
   const handleFileButtonClick = () => {
-    fileInputRef.current?.click();
+    fileInputRef.current?.click(); // 파일 입력 창을 엽니다.
   };
   const router = useRouter();
 
@@ -78,7 +66,7 @@ export default function Page() {
       alert('시간이 아직 남아있습니다.');
       return;
     }
-    await apiClient.post(
+    const res: any = await apiClient.post(
       `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/email/registration/request`,
       { email: editEmail },
       {
@@ -120,7 +108,7 @@ export default function Page() {
 
   const onSubmitHandler = async () => {
     try {
-      await apiClient.post(
+      const res: any = await apiClient.post(
         `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/member/info`,
         { nickName: editNickname, profileImg: imgFile },
         {
@@ -145,214 +133,215 @@ export default function Page() {
       setImgFile(file);
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImg(reader.result as string);
+        setImg(reader.result as string); // 파일 내용을 상태로 설정합니다.
       };
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(file); // 파일 내용을 Data URL로 읽습니다.
     }
   };
 
   return (
-    <Paper elevation={0} style={{ minHeight: '100vh', padding: '20px' }}>
-      <Grid container justifyContent="center" spacing={4}>
-        <Grid item xs={12} md={8}>
-          <Card>
-            <CardContent>
-              <Grid container spacing={3} alignItems="center">
-                <Grid item>
-                  <Badge
-                    overlap="circular"
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                    badgeContent={
-                      edit && (
-                        <IconButton
-                          color="primary"
-                          component="label"
-                          onClick={handleFileButtonClick}
-                        >
-                          <PhotoCamera />
-                          <input
-                            ref={fileInputRef}
-                            type="file"
-                            hidden
-                            onChange={handleFileChange}
-                          />
-                        </IconButton>
-                      )
+    <div
+      className="relative flex  min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden mx-auto"
+      style={{ fontFamily: 'Inter, Noto Sans, sans-serif' }}
+    >
+      <div className="layout-container flex h-full grow flex-col">
+        <div className="flex flex-1 justify-center py-5">
+          <div className="layout-content-container mx-auto flex flex-col min-w-[960px] flex-1 items-center">
+            <div className="flex p-4 @container justify-center">
+              <div className="flex w-full flex-col gap-4 items-start">
+                <div className="flex gap-4 flex-col items-start">
+                  <img
+                    alt="avatar"
+                    className="bg-center bg-no-repeat aspect-square bg-cover rounded-full min-h-32 w-32"
+                    src={
+                      img
+                        ? img
+                        : 'https://cdn.usegalileo.ai/stability/77b57bfe-1501-4498-b8a4-24d719383033.png'
                     }
-                  >
-                    <Avatar
-                      src={
-                        img
-                          ? img
-                          : 'https://cdn.usegalileo.ai/stability/77b57bfe-1501-4498-b8a4-24d719383033.png'
-                      }
-                      alt="avatar"
-                      sx={{ width: 128, height: 128 }}
-                    />
-                  </Badge>
-                </Grid>
-                <Grid item xs>
-                  {edit ? (
-                    <TextField
-                      label="닉네임"
-                      variant="outlined"
-                      fullWidth
-                      value={editNickname}
-                      onChange={onNicknameChangeHandler}
-                    />
-                  ) : (
-                    <Typography variant="h5">닉네임: {nickname}</Typography>
-                  )}
-                  <Typography variant="subtitle1">이메일: {email}</Typography>
-                </Grid>
-                <Grid item>
-                  {edit ? (
-                    <IconButton color="primary" onClick={onSubmitHandler}>
-                      <Save />
-                    </IconButton>
-                  ) : (
-                    <IconButton color="primary" onClick={() => setEdit(true)}>
-                      <Edit />
-                    </IconButton>
-                  )}
-                </Grid>
-              </Grid>
-            </CardContent>
-            {postEmail && (
-              <CardContent>
-                <Grid container spacing={2} alignItems="flex-end">
-                  <Grid item xs={12} sm={8}>
-                    <TextField
-                      label="이메일"
-                      variant="outlined"
-                      fullWidth
-                      disabled={count}
-                      value={editEmail}
-                      onChange={onEmailChangeHandler}
-                      error={emailError}
-                      helperText={emailError ? '이메일을 입력해주세요!' : ''}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={4}>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      fullWidth
-                      onClick={emailCheckHandler}
-                      disabled={count}
-                    >
-                      인증하기
-                    </Button>
-                  </Grid>
-                </Grid>
-                {count && countdown > 0 && (
-                  <Typography color="error" variant="body2" sx={{ mt: 2 }}>
-                    남은 시간: {formatTime(countdown)}
-                  </Typography>
-                )}
-                {!count && countdown === 0 && (
-                  <Typography color="error" variant="body2" sx={{ mt: 2 }}>
-                    이메일이 만료되었습니다. 다시 인증해주세요!
-                  </Typography>
-                )}
+                  />
+                  <div className="flex flex-col justify-center">
+                    <p className="text-[#0e141b] text-[22px] font-bold leading-tight tracking-[-0.015em]">
+                      닉네임 : {nickname}
+                    </p>
+                    <p className="text-[#0e141b] text-[22px] font-bold leading-tight tracking-[-0.015em]">
+                      이메일 : {email}
+                    </p>
+                  </div>
+                </div>
                 <Button
                   variant="contained"
-                  sx={{ mt: 2 }}
-                  onClick={() => setPostEmail(false)}
+                  className="w-[480px]"
+                  onClick={() => router.push('/list-product-sell')}
                 >
+                  판매자 - 내 판매 제품 목록 확인하기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/withdraw')}
+                >
+                  회원탈퇴하기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/review')}
+                >
+                  내 판매 정산 보기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/review/cost')}
+                >
+                  내 판매 정산액 보기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/list-refund-buyer')}
+                >
+                  구매자 - 환불 요청 목록 확인하기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/list-refund')}
+                >
+                  판매자 - 환불 요청 목록 확인하기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/list-buy')}
+                >
+                  구매 목록 확인하기
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/bank')}
+                >
+                  계좌 등록 및 조회
+                </Button>
+                <Button
+                  variant="contained"
+                  className="w-[480px]"
+                  onClick={() => router.push('/chat')}
+                >
+                  채팅방 확인하기
+                </Button>
+                {!edit ? (
+                  <button
+                    onClick={() => setEdit(true)}
+                    className="flex  cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#e7edf3] text-[#0e141b] text-sm font-bold leading-normal tracking-[0.015em] w-full min-w-[480px] @[480px]:w-auto"
+                  >
+                    <span className="truncate">프로필 수정하기</span>
+                  </button>
+                ) : (
+                  <></>
+                )}
+                {postEmail === false && edit === false && (
+                  <Button
+                    className="w-[480px] bg-red-300"
+                    variant="contained"
+                    onClick={() => setPostEmail(true)}
+                  >
+                    이메일 등록
+                  </Button>
+                )}
+              </div>
+            </div>
+
+            {postEmail && (
+              <div>
+                <div className="flex min-w-[480px] flex-wrap items-end gap-4 px-4 py-3 relative">
+                  <label className="flex flex-col min-w-40 flex-1">
+                    <p className="text-[#0e141b] text-base font-medium leading-normal pb-2">
+                      Email
+                    </p>
+                    <input
+                      disabled={count ? true : false}
+                      onChange={onEmailChangeHandler}
+                      value={editEmail}
+                      className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e141b] focus:outline-0 focus:ring-0 border border-[#d0dbe7] bg-slate-50 focus:border-[#d0dbe7] h-14 placeholder:text-[#4e7397] p-[15px] text-base font-normal leading-normal"
+                    />
+                  </label>
+                  <p
+                    onClick={emailCheckHandler}
+                    className="absolute right-[35px] bottom-[25px] cursor-pointer text-blue-500"
+                  >
+                    인증하기
+                  </p>
+                </div>
+                <Button variant="contained" onClick={() => setPostEmail(false)}>
                   저장하고 나가기
                 </Button>
-              </CardContent>
+              </div>
             )}
-            <CardActions sx={{ flexWrap: 'wrap', gap: 1 }}>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/list-product-sell')}
-              >
-                판매자 - 내 판매 제품 목록 확인하기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/withdraw')}
-              >
-                회원탈퇴하기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/review')}
-              >
-                내 판매 정산 보기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/review/cost')}
-              >
-                내 판매 정산액 보기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/list-refund-buyer')}
-              >
-                구매자 - 환불 요청 목록 확인하기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/list-refund')}
-              >
-                판매자 - 환불 요청 목록 확인하기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/list-buy')}
-              >
-                구매 목록 확인하기
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/bank')}
-              >
-                계좌 등록 및 조회
-              </Button>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => router.push('/chat')}
-              >
-                채팅방 확인하기
-              </Button>
-              {!edit && (
-                <Button
-                  variant="outlined"
-                  fullWidth
-                  onClick={() => setEdit(true)}
-                  startIcon={<Edit />}
-                >
-                  프로필 수정하기
-                </Button>
-              )}
-              {postEmail === false && edit === false && (
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  fullWidth
-                  startIcon={<Email />}
-                  onClick={() => setPostEmail(true)}
-                >
-                  이메일 등록
-                </Button>
-              )}
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </Paper>
+
+            {count && countdown > 0 && (
+              <p className="text-red-500">남은 시간: {formatTime(countdown)}</p>
+            )}
+
+            {!count && countdown === 0 && (
+              <p className="text-red-500">
+                이메일이 만료되었습니다. 다시 인증해주세요!
+              </p>
+            )}
+
+            {emailError && (
+              <p className="text-red-500">이메일을 입력해주세요!</p>
+            )}
+
+            {edit ? (
+              <>
+                <div className="flex min-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                  <div className="flex flex-col min-w-40 flex-1">
+                    <p className="text-[#0e141b] text-base font-medium leading-normal pb-2">
+                      Nickname 변경하기
+                    </p>
+                    <input
+                      type="text"
+                      onChange={onNicknameChangeHandler}
+                      value={editNickname}
+                      className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e141b] focus:outline-0 focus:ring-0 border border-[#d0dbe7] bg-slate-50 focus:border-[#d0dbe7] h-14 placeholder:text-[#4e7397] p-[15px] text-base font-normal leading-normal"
+                    />
+                  </div>
+                  <input
+                    ref={fileInputRef}
+                    type="file"
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }} // 파일 입력을 숨깁니다.
+                  />
+                  <button
+                    onClick={handleFileButtonClick}
+                    className="flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0e141b] focus:outline-0 focus:ring-0 border border-[#d0dbe7] bg-slate-50 focus:border-[#d0dbe7] h-14 p-[15px] text-base font-normal leading-normal cursor-pointer"
+                  >
+                    프로필 이미지 변경하기
+                  </button>
+                </div>
+
+                <div className="flex px-4 py-3">
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      setEdit(false);
+                      onSubmitHandler();
+                    }}
+                    disabled={count}
+                    className={`flex w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 ${!count ? 'bg-[#1980e6] text-slate-50' : 'bg-gray-300 text-white'} text-base font-bold leading-normal tracking-[0.015em]`}
+                  >
+                    <span className="truncate">Save Changes</span>
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
